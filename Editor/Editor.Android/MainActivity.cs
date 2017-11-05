839ac915d3a478cba761663bbdd253b04a07d788
Michał Rychlewicz <michal.rychlewicz@edu.uekat.pl>
2017-11-05 12:54:50 +0100

    Picking from the gallery and displaying an image.

--- Editor/Editor.Android/MainActivity.cs
+++ Editor/Editor.Android/MainActivity.cs
@@ -1,26 +1,50 @@
 ﻿using System;
-
+using System.IO;
+using System.Threading.Tasks;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Editor.Droid
 {
     [Activity(Label = "Editor", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity :/* global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity*/ global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            
 
-            base.OnCreate(bundle);
+           base.OnCreate (bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            global::Xamarin.Forms.Forms.Init (this, bundle);
+
+            LoadApplication (new App ());
+        }
+
+        public static readonly int PickImageId = 1000;
+
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { set; get; }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = ContentResolver.OpenInputStream(uri);
+
+                    // Set the Stream as the completion of the Task
+                    PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
         }
     }
 }
--- Editor/Editor/MainPage.xaml.cs
+++ Editor/Editor/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+
 
 namespace Editor
 {
@@ -11,7 +13,57 @@ namespace Editor
     {
         public MainPage()
         {
-            InitializeComponent();
+            Button pickImage;
+            
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 1
+            };
+            panel.Children.Add(new Label
+            {
+                Text = "Select an image",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalTextAlignment = TextAlignment.Start,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+
+            });
+            
+            panel.Children.Add(pickImage = new Button
+            {
+                Text = "Click to select image",
+                VerticalOptions = LayoutOptions.End,
+                
+               
+            });
+            
+            pickImage.Clicked += async (sender, e) =>
+            {
+                pickImage.IsEnabled = true;
+                Stream stream = await DependencyService.Get<IPicturePicker>().GetImageStreamAsync();
+                if (stream != null)
+                {
+                    
+                    //panel.Children[0]  = new Image
+                    //{
+                    //    Source = ImageSource.FromStream(() => stream),
+                    //    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    //    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    //    Aspect = Aspect.AspectFill
+
+                    //};    
+                    Content = new Image{Source = ImageSource.FromStream(() => stream) };
+                }
+                
+            };
+            this.Content = panel;
+            
+
+
         }
+        
+        
+        
+
     }
 }
